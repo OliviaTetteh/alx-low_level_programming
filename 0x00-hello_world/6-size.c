634d6822ef7a5b32c3e784dbc3142aa4be984f01
OliviaTetteh <olivet36@yahoo.com>
2023-08-13 01:05:51 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
 printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(long
 long int));
 printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(float));
-return(0);
+return (0);
 }
