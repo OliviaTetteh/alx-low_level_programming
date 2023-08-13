676856dbe884f895d9da81ccc815e54ba8d39e58
OliviaTetteh <olivet36@yahoo.com>
2023-08-12 19:15:20 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - program that the size of types of computers
+ *
+ *Return: always 0 (success)
+ */
+int main (void)
+{
+printf("size of char: %lubyte(s)\n", size of (char));
+printf("size of int: %lubyte(s)\n", size of (int));
+printf("size of long int: %lubyte(s)\n", size of (long int));
+printf("size of long long int: %lubyte(s)\n", size of (long long int));
+printf("size of float: %lubyte(s)\n", size of (float));
+Return (0);
+}
