7ebcf24adef8f8edca376208336dacf1a207f079
OliviaTetteh <olivet36@yahoo.com>
2023-08-15 17:12:43 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - printing of _putchar followed by a new liine
+ * Return: 0
+ */
+int main(void)
+{
+printf("_putcher(\n");
+return (0);
+}
