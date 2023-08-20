c1238e38749f62eb79dd19f75ef17c57395ed3c7
OliviaTetteh <olivet36@yahoo.com>
2023-08-20 04:12:43 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - This code checks if a letter is uppercase
+ * @x: The number to be checked
+ * return: 1 for uppercase letter or 0 for any else
+ */
+
+int _isupper(int x)
+
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
