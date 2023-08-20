8c785c79b644790fcdb46aed4b6add236e48ba45
OliviaTetteh <olivet36@yahoo.com>
2023-08-20 08:38:49 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,28 +2,28 @@
 
 /**
  * print_triangle - This prints a triangle of squares according to parameter
- * @b: The size of the squares triangle
+ * @size: The size of the squares triangle
  * Return: empty
  */
 
-void print_triangle(int b)
+void print_triangle(int size)
 
 {
 	int x, y, z;
 
-	if (b <= 0)
+	if (size <= 0)
 	{
 	_putchar('\n');
 	}
 	else
 	{
-	for (x = 0; x < b; x++)
+	for (x = 0; x < size; x++)
 	{
-	for (y = b -  x; y > 1; y--)
+	for (y = size -  x; y > 1; y--)
 	{
 	_putchar(32);
 	}
-	for (z = 0; z <= x; x++)
+	for (z = 0; z <= x; z++)
 	{
 	_putchar(35);
 	}
