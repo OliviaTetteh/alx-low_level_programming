610fb141f668ace1f5b7e6a6c9f8f3d8ecd82e88
OliviaTetteh <olivet36@yahoo.com>
2023-08-20 06:19:27 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - This code prints 10 times the numbers from 0 up to 14
- * Return: 10 times of the numbers since 0 up to 14
+ * Return: 10 times of the numbers from 0 up to 14
  */
 
 void more_numbers(void)
@@ -16,9 +16,9 @@ void more_numbers(void)
 	if (y > 9)
 	{
 	_putchar((y / 10) + '0');
+	}
 	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
-	}
 	}
 }
