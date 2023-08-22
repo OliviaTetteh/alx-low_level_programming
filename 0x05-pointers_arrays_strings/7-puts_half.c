e6424447c915c3c3073cce8c67e14fd62bfa8401
OliviaTetteh <olivet36@yahoo.com>
2023-08-22 07:38:11 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - This prints the remaining of a string
+ * @str: It represents the string input
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int half, i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	half = (len - 1) / 2;
+
+	for (i = (half + 1); i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+}
