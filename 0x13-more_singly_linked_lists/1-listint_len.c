7bef3afdffbf57013a54478e99a7f8a2a2b95a4f
OliviaTetteh <olivet36@yahoo.com>
2023-09-26 02:17:25 +0000

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,8 @@
-#include "lists.h"
-
 /**
  * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t 
+ * @h: linked list of type listint_t
  *
- * Return: number of nodes
+ * Return: the number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
